661324d448bb5bd8cfcbcb98e03d07422c1c79c3
higuko <novavita482@gmail.com>
2026-09-15 23:01:46 +0300

    fefwfe

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import cv2
+import numpy as np
+import urllib.request
+import os
+
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-model_path = 'blaze_face_short_range.tflite'
+
+MODEL_PATH = 'face_landmarker.task'
+MODEL_URL = "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task"
+
+if not os.path.exists(MODEL_PATH):
+    print("Скачиваю модель face_landmarker.task...")
+    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+    
+base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+options = vision.FaceLandmarkerOptions(
+    base_options=base_options,
+    running_mode=vision.RunningMode.VIDEO,
+    num_faces=1,
+    min_face_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    output_face_blendshapes=False,
+    output_facial_transformation_matrixes=False,
+)
+detection = vision.FaceLandmarker.create_from_options(options)
